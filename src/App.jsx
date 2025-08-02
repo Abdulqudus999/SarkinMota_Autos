e3f871801a4437e798b1a3e8a01c1c661f49d4c7
Abdulqudus999 <abdulsalamolaide958@gmail.com>
2025-08-03 00:58:20 +0100

    adjustment 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import AboutUs from './pages/AboutUs'
 import OurCars from './pages/OurCars'
@@ -8,14 +8,14 @@ import ContactUs from './pages/ContactUs'
 const App = () => {
   return (
     <>
-    <HashRouter>
+    <BrowserRouter basename="/SarkinMota_Autos">
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/aboutus' element={<AboutUs />}/>
       <Route path='/ourcars' element={<OurCars />}/>
       <Route path='/contactus' element={<ContactUs />}/>
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
     
     
     </>
